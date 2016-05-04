06e47bcb33c8e01b632183941184e53b02f0ed8b
Peter Eichinger <peter.eichinger@gmail.com>
2016-05-04 16:48:06 +0200

    Add ABC for point sources and a sample sine point source

---
+++ Assets/Scripts/PointSource/AbstractPointSource.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TrailAdvanced.PointSource {
+	public abstract class AbstractPointSource : MonoBehaviour {
+		public abstract Vector3[] GetPoints();
+	}
+}
---
+++ Assets/Scripts/PointSource/SinePointSource.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrailAdvanced.PointSource {
+
+	public class SinePointSource : AbstractPointSource {
+		public float frequency = 1f;
+		public float amplitude = 1f;
+		public int quality = 1;
+
+		private List<Vector3> points;
+		private float _time;
+
+		private void Start() {
+			points = new List<Vector3>();
+		}
+
+		private void Update() {
+			float newTime = _time + Time.deltaTime;
+			for (int i = 0; i <= quality; i++) {
+				float t = Mathf.Lerp(_time, newTime, (float)i / quality);
+				points.Add(new Vector3(t, amplitude * Mathf.Sin(frequency * t), 0));
+			}
+			_time = newTime;
+		}
+
+		public override Vector3[] GetPoints() {
+			return points.ToArray();
+		}
+	}
+}
