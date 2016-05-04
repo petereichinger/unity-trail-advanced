8cd591c6816e5d8ce13418dae0393f82d534315c
Peter Eichinger <peter.eichinger@gmail.com>
2016-05-04 23:29:42 +0200

    Add points to front and only add if moved a little bit

--- Assets/Scripts/PointSource/ObjectPointSource.cs
+++ Assets/Scripts/PointSource/ObjectPointSource.cs
@@ -5,10 +5,22 @@ namespace TrailAdvanced.PointSource {
 
 	public class ObjectPointSource : AbstractPointSource {
 		public Transform objectToFollow;
+		private Vector3 _lastPosition;
+
+		protected override void Start() {
+			base.Start();
+			_lastPosition = objectToFollow.position;
+		}
 
 		protected override void Update() {
 			base.Update();
-			points.AddToBack(objectToFollow.position);
+
+			float distance = Vector3.SqrMagnitude(objectToFollow.position - _lastPosition);
+
+			if (distance > 0.0001f) {
+				points.AddToFront(objectToFollow.position);
+			}
+			_lastPosition = objectToFollow.position;
 		}
 	}
 }
