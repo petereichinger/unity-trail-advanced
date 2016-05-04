8ba6063d045170e97b44f0c2efa5628ae66ba05d
Peter Eichinger <peter.eichinger@gmail.com>
2016-05-04 20:34:25 +0200

    Add source that follows a transform

---
+++ Assets/Scripts/PointSource/ObjectPointSource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace TrailAdvanced.PointSource {
+
+	public class ObjectPointSource : AbstractPointSource {
+		public Transform objectToFollow;
+
+		protected override void Update() {
+			base.Update();
+			points.AddToBack(objectToFollow.position);
+		}
+	}
+}
