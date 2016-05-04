479a8a2fe0abdb065d683f2c64124b20f7d3e99b
Peter Eichinger <peter.eichinger@gmail.com>
2016-05-04 23:30:53 +0200

    Script to move a game object around

---
+++ Assets/TestMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+	public float speed;
+	private Transform _t;
+
+	private void Start() {
+		_t = transform;
+	}
+
+	// Update is called once per frame
+	private void Update() {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		_t.position += new Vector3(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime);
+	}
+}
